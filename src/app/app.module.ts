800d17d2f96be21ed419b07cf308a9cc4c59527f
NEXUS\zha202 <yang.zhang@data61.csiro.au>
2017-11-13 13:56:29 +1100

    Added example of loading options from remote server.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpModule} from '@angular/http';
 
 import { SelectModule } from './ng-select/ng-select.module';
 
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     SelectModule
   ],
   providers: [],
